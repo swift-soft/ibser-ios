06c67749fa34f54e24a68f30f4e59cf073b9f636
247033 Wojciech Mokwiński <247033@edu.p.lodz.pl>
2023-10-26 21:08:02 +0200

    Initial Commit

---
+++ IBSER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBSER
+//
+//  Created by Wojciech Mokwiński on 26/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
